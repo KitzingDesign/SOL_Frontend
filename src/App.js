348baf1c77b31aa2811c727bb637f3a202e33fb7
KitzingDesign <jakob.kitzing@hotmail.com>
2024-08-13 13:32:07 +0200

    Branch for place card feature created

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ const DUMMY_DATA = [
 		open: true,
 		img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiCQvnzArXzlI9ACZ1NGaKvgVQ6wwz4Ll135FIPWOyn4Gm1zdHBYymxQeOlrep1A7mzkv_YwLZfJQARt3BjFbj6_o5oNc4FOXhBfGH-tBnYdNwMOUPEafwS7fHgYs-f0nE_4pc2M4Oac6k/s1600/IMG_20200503_174753664_HDR.jpg",
 	},
+	{
+		title: "Stora teatern",
+		rating: 9.2,
+		distance: 3,
+		area: "Kungsportsplatsen",
+		sun: ["15.00", "02.00"],
+		open: true,
+		img: "https://cms.goteborg.com/uploads/2021/03/storkoket-superstudio-43-2500x1874.jpg",
+	},
 ];
 
 function App() {
